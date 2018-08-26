17e827276a56ae84bb2e1aeb374a737ace5855f2
Tapani Raunio <traunio@users.noreply.github.com>
2018-08-26 13:42:30 +0300

    Initial commit

---
+++ webapp.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, g
+from puoliintumisajat.decay_webapp import decay_webapp
+from vuorotiheys.vuorotiheys import vuorotiheys
+
+app = Flask(__name__)
+
+app.register_blueprint(decay_webapp, url_prefix='/atomit')
+app.register_blueprint(vuorotiheys, url_prefix='/vuoro')
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Sorry, sivua ei löytynyt", 404
+    
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+if __name__ == '__main__':
+    app.run()
+
