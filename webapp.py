2064198e8b5787d892a25912cb0705be630ce98d
Tapani Raunio <traunio@users.noreply.github.com>
2018-08-28 21:26:10 +0300

    Add index.html

--- webapp.py
+++ webapp.py
@@ -8,6 +8,7 @@ app.register_blueprint(decay_webapp, url_prefix='/atomit')
 app.register_blueprint(vuorotiheys, url_prefix='/vuoro')
 
 @app.route('/')
+@app.route('/index.html')
 def homepage():
     return render_template('homepage.html')
 
